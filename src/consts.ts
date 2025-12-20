020716e1901aeac67fd408f39a64adeefbe9c461
Michal Dobrzycki <dobrzycki@gmail.com>
2025-12-20 14:51:28 +0100

    Poprawiona treść strony

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,24 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Astro Blog";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_TITLE = "Stowarzyszenie Nadolice Wielkie";
+export const SITE_DESCRIPTION = "Stowarzyszenie działające na rzecz rozwoju wsi Nadolice Wielkie";
+
+export const ORG_INFO = {
+	fullName: "STOWARZYSZENIE NADOLICE WIELKIE",
+	krs: "0000540080",
+	nip: "8961541184",
+	regon: "360614482",
+	address: "Agrestowa 9, Nadolice Wielkie, 55-003 Czernica, Polska",
+	legalForm: "STOWARZYSZENIE",
+	registrationDate: "21 stycznia 2015 r.",
+	board: {
+		president: "Maciej Pakulski",
+		treasurer: "Bogusława Władyczka",
+		vicePresident: "Barbara Domowicz",
+		boardMember: "Michał Dobrzycki"
+	},
+	socialMedia: {
+		facebook: "https://www.facebook.com/stowarzyszenieNADOLICE/?locale=pl_PL"
+	}
+};
